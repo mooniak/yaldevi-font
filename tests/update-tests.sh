6177ab0ff341e8470e29fcaf2a7e68ffcb482404
Pathum Egodawatta <pathumego@gmail.com>
2015-07-06 00:02:16 +0700

    Updating Tests Doc

---
+++ tests/update-tests.sh
@@ -0,0 +1,6 @@
+cp /Users/Pathum/AyannaNarrowTamil-Light.otf  /Users/Pathum/src/github.com/pathumego/ayanna-narrow/tests/fonts
+cp /Users/Pathum/AyannaNarrowTamil-ExtraBold.otf  /Users/Pathum/src/github.com/pathumego/ayanna-narrow/tests/fonts
+cd /Users/Pathum/src/github.com/pathumego/ayanna-narrow/tests/
+git add .
+git commit -m 'Updating Tests Doc'
+git push
