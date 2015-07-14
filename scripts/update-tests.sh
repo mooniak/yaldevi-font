9f16ad5e39113a9abb4eb7de43c17b97df97b340
Pathum Egodawatta <pathumego@gmail.com>
2015-07-15 01:26:54 +0530

    Revert 4516c04..f391a4e
    This rolls back to commit 4516c046ac9ca0759a75f75cd6ad3619f05ed63f.

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-cd sources/
+#Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
+cd ../sources/
 
 fontforge ../scripts/fontconvert AyannaNarrowSinhala-Light.sfd --otf
 fontforge ../scripts/fontconvert AyannaNarrowSinhala-ExtraBold.sfd --otf
-fontforge ../scripts/fontconvert AyannaNarrowTamil-ExtraBold.sfd --otf
 fontforge ../scripts/fontconvert AyannaNarrowTamil-Light.sfd --otf
+fontforge ../scripts/fontconvert AyannaNarrowTamil-ExtraBold.sfd --otf
 
-kill
-
-mv *otf tests/fonts/
-cd tests/
+mv *otf ../tests/fonts
+cd ../tests/
 git add .
 git commit -m 'Updating Tests Doc'
