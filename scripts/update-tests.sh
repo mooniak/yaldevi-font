3aaf826c9e2fc83164fd1e507fdfde080fafba6f
Pathum Egodawatta <pathumego@gmail.com>
2015-07-15 00:19:54 +0530

    Updating Tests Doc

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
 cd ../sources/
 
 fontforge ../scripts/fontconvert AyannaNarrowSinhala-Light.sfd --otf
 fontforge ../scripts/fontconvert AyannaNarrowSinhala-ExtraBold.sfd --otf
 fontforge ../scripts/fontconvert AyannaNarrowTamil-ExtraBold.sfd --otf
 fontforge ../scripts/fontconvert AyannaNarrowTamil-Light.sfd --otf
+
 
 mv *otf ../tests/fonts
 cd ../tests/
