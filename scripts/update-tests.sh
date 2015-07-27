2010453843e1186275a4fa520f9fc7a688d8a2ea
Pathum Egodawatta <pathumego@gmail.com>
2015-07-27 18:25:34 +0530

    Updates

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -4,8 +4,8 @@ cd ../sources/
 
 fontforge ../scripts/fontconvert AyannaNarrowSinhala-Light.sfd --otf --add-extrema --correct-directions --remove-overlap --round-points
 fontforge ../scripts/fontconvert AyannaNarrowSinhala-ExtraBold.sfd --otf --add-extrema --correct-directions --remove-overlap --round-points
-fontforge ../scripts/fontconvert AyannaNarrowTamil-Light.sfd --otf --add-extrema --correct-directions --remove-overlap --round-points
-fontforge ../scripts/fontconvert AyannaNarrowTamil-ExtraBold.sfd --otf --add-extrema --correct-directions --remove-overlap --round-points
+fontforge ../scripts/fontconvert AyannaNarrowTamil-Light.sfd --otf --correct-directions --remove-overlap --round-points
+fontforge ../scripts/fontconvert AyannaNarrowTamil-ExtraBold.sfd --otf --correct-directions --remove-overlap --round-points
 
 mv *otf ../tests/fonts
 cd ../tests/
