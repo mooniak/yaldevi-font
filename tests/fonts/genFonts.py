74bd075ad3274a6ded5d55916af8599333a8fe20
Pathum Egodawatta <pathumego@gmail.com>
2015-08-17 17:28:56 +0530

    updates

--- tests/fonts/genFonts.py
+++ tests/fonts/genFonts.py
@@ -4,19 +4,19 @@ import os, sys
 
 def genFonts(fileName, outputDir):
     os.system('mkdir -p ' + outputDir)
-    commits = getGitCommits(fileName).reverse()
-
-    for commitHash in commits:
-        os.system('git checkout ' + commitHash)
-        outputFileName = str('%05d' + fileName)
-        os.system('cp ' + fileName + ' ' + outputDir +
-        outputFileName)
-        os.sytem('rm log')
-
-def getGitCommits(fileName):
     os.system('git log --pretty=%h ' + fileName + ' > log')
     commits = open('log').read().splitlines()
-    return commits
+    print(commits)
+    
+    try:
+        for commitHash in commits:
+            os.system('git checkout ' + commitHash)
+            outputFileName = str('%05d' + fileName)
+            os.system('cp ' + fileName + ' ' + outputDir +
+            outputFileName)
+            os.sytem('rm log')
+    except:
+        print("log file read error or no git commits...")
 
 def main(argv):
     genFonts(argv[1], argv[2])
