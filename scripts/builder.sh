7066990ac4b5ff30dd683d67524ce928327ffcdd
paarandika <paarandika@gmail.com>
2015-11-27 13:48:50 +0530

    Updated builder.sh

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -14,12 +14,13 @@ cd ../sources/sfd
 
 python ../../scripts/fontconvert Ayanna-sinhala-0.sfd ../../sources --ufo
 python ../../scripts/fontconvert Ayanna-sinhala-1.sfd ../../sources --ufo
+python ../../scripts/fontconvert Ayanna-sinhala-1-opt.sfd ../../sources --ufo
 python ../../scripts/fontconvert Ayanna-tamil-0.sfd ../../sources --ufo
 python ../../scripts/fontconvert Ayanna-tamil-1.sfd ../../sources --ufo
 
 cd ../../scripts
 python merger.py ../masters/Ayanna-Regular.ufo ../sources/Ayanna-sinhala-0.ufo ../sources/Ayanna-latin-0.ufo ../sources/Ayanna-tamil-0.ufo
 python merger.py ../masters/Ayanna-ExtraBold.ufo ../sources/Ayanna-sinhala-1.ufo ../sources/Ayanna-latin-1.ufo ../sources/Ayanna-tamil-1.ufo
-
+python merger.py ../masters/Ayanna-Bold ../sources/Ayanna-sinhala-1-opt.ufo
 cd ../
 python build.py
