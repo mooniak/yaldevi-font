40052fdaa331dfc77ed7b45cd9f3672396d7afb2
paarandika <paarandika@gmail.com>
2016-03-08 20:59:25 +0530

    changed rm command

--- scripts/gfbuilder.sh
+++ scripts/gfbuilder.sh
@@ -11,17 +11,16 @@
 #!/bin/bash
 
 cd ../sources/sfd
-rm -rf ../Ayanna-sinhala-0.ufo ../Ayanna-sinhala-1.ufo ../Ayanna-sinhala-1-opt.ufo ../Ayanna-tamil-0.ufo ../Ayanna-tamil-1.ufo
+rm -R -f ../Ayanna-sinhala-0.ufo ../Ayanna-sinhala-1.ufo ../Ayanna-sinhala-1-opt.ufo ../Ayanna-tamil-0.ufo ../Ayanna-tamil-1.ufo
 python ../../scripts/fontconvert Ayanna-sinhala-0.sfd ../../sources --ufo
 python ../../scripts/fontconvert Ayanna-sinhala-1.sfd ../../sources --ufo
 python ../../scripts/fontconvert Ayanna-sinhala-1-opt.sfd ../../sources --ufo
 python ../../scripts/fontconvert Ayanna-tamil-0.sfd ../../sources --ufo
 python ../../scripts/fontconvert Ayanna-tamil-1.sfd ../../sources --ufo
 
-rm -rf ../ttf-build
-
 cd ../../scripts
-rm -rf ../masters/*.ufo
+rm -R -f ../ttf-build
+rm -R -f ../masters/*.ufo
 python merger.py ../masters/Ayanna-Regular.ufo ../sources/Ayanna-sinhala-0.ufo ../sources/Ayanna-latin-0.ufo
 python merger.py ../masters/Ayanna-ExtraBold.ufo ../sources/Ayanna-sinhala-1.ufo ../sources/Ayanna-latin-1.ufo
 python merger.py ../masters/Ayanna-Bold.ufo ../sources/Ayanna-sinhala-1-opt.ufo ../sources/Ayanna-latin-1.ufo
@@ -29,7 +28,7 @@ cd ../
 python gfbuild-s.py
 
 cd scripts
-rm -rf ../masters/*.ufo
+rm -R -f ../masters/*.ufo
 python merger.py ../masters/Ayanna-Regular.ufo ../sources/Ayanna-tamil-0.ufo ../sources/Ayanna-latin-0.ufo
 python merger.py ../masters/Ayanna-ExtraBold.ufo ../sources/Ayanna-tamil-1.ufo ../sources/Ayanna-latin-1.ufo
 cd ../
